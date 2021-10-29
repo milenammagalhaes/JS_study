f26d1aded57c6d0297531fc10586c02cc94895b0
Milena Martins <milenammagalhaes@gmail.com>
2021-10-28 21:20:22 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+// object literals
+let user = {
+  name: `milena`,
+  age: 24,
+  location: `Montreal`,
+  nicknames: [`Mi`, `mimi`, `mimica`],
+  login: (name) => {
+    console.log(`the user logged in`, name)
+  },
+  logout: () => {
+    console.log(`the user logged out`)
+  },
+}
+// console.log(user.nicknames)
+// user.age = 25
+// console.log(user)
+// console.log(typeof user.age)
+user.login(user.name)
+user.logout(user.name)
+const name = `rafael`
+name.toUpperCase()
+
